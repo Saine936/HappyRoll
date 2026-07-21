cb9c8562ee30e3c488aa6df0fcbce68f0cf3eb86
Hussein Mrad <mrad.hussein.200@gmail.com>
2026-07-21 02:25:59 -0400

    Fix HappyRoll GitHub Pages deployment

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '@/App';
-import '@/index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "@/App";
+import "@/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 window.requestAnimationFrame(() => {
-	setTimeout(() => {
-		const splash = document.getElementById('app-splash');
-		if (splash) {
-			splash.classList.add('hide');
-			setTimeout(() => splash.remove(), 600);
-		}
-	}, 400);
+  setTimeout(() => {
+    const splash = document.getElementById("app-splash");
+
+    if (splash) {
+      splash.classList.add("hide");
+
+      setTimeout(() => {
+        splash.remove();
+      }, 600);
+    }
+  }, 400);
 });
